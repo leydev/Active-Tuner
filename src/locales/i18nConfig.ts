e04c5e5b8c989113a02c7edf18cc93fb21c0c48a
ayelsew <wesley.waaraujo@gmail.com>
2022-01-23 16:04:26 -0300

    detect lang browser

--- src/locales/i18nConfig.ts
+++ src/locales/i18nConfig.ts
@@ -4,13 +4,17 @@ import { initReactI18next } from 'react-i18next';
 import pt from './pt.json';
 import en from './en.json';
 
+const lang = new URLSearchParams(window.location.search).get('lang');
+
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 i18n.use(initReactI18next)
   .init({
     resources: {
       pt: { translation: pt },
       en: { translation: en },
+      'pt-BR': { translation: pt },
     },
+    lng: lang || navigator.language,
     fallbackLng: 'en',
     interpolation: {
       escapeValue: true,
